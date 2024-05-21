91a10c6ecffe26069c65c28f81896de8135eb67e
Vítor Viana <vitorviana@id.uff.br>
2024-05-20 21:00:13 -0300

    Dados de acesso do SMTP

--- back_end/api/config/email_config.py
+++ back_end/api/config/email_config.py
@@ -1,5 +1,5 @@
 class EmailConfig:
     SMTP_SERVER = "smtp.gmail.com"
     SMTP_PORT = 587
-    SMTP_USERNAME = "v2web.agencia@gmail.com"
-    SMTP_PASSWORD = "jlnt bsvr oibv pups"
+    SMTP_USERNAME = "sendform4@gmail.com"
+    SMTP_PASSWORD = "w q g n h z v x c m t i m g o a"
